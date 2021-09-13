b4504c085a1a329e869cf996d3e38305cf054488
cclund444 <87826114+cclund444@users.noreply.github.com>
2021-09-12 23:34:59 -0600

    Add files via upload
    Start of page

---
+++ script.js
@@ -0,0 +1,12 @@
+var submitBtn = document.querySelector("#submit");
+var selectLocation = document.querySelector("#location-selecy");
+var selectDays = document.querySelector("date-select");
+
+var getLocation = function(location) {
+    // add info and api method here 
+}
+
+var getWeather = function(data) {
+    // add info and api method here
+    var apiUrl = ; 
+}
