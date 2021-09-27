0248a051b50688527b4940daaab3b9b90c0ec4ae
cclund444 <87826114+cclund444@users.noreply.github.com>
2021-09-26 21:40:38 -0600

    update

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ var getLocation = function(location) {
 }
 
 var getWeather = function(data) {
-    // add info and api method here
-    var apiUrl = ; 
+    // add info and api method here--check java on project 1 for help
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.data[0].latitude + "&lon=" + data.data[0].longitude + "&units=imperial&appid=d3f5af43f561d831f34569cf6fef321f"); 
+    .then(function(response) {
+        return response.json();
+    })
 }
 
 var input = document.querySelector("#weather")
